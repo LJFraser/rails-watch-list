2a4f2efc51daae6cf2ec1f5253ddc022dfcd4c1e
Leanne Fraser <leannejf@gmail.com>
2024-06-06 17:04:47 +0200

    parts 1-4

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+     @bookmark = Bookmark.find(params[:id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(list_params)
+    @bookmark.list = List.find(params[:list_id])
+    if @bookmark.save
+      redirect_to @bookmark.list, notice: 'Bookmark was successfully created'
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+end
+
+private
+
+def set_bookmark
+  @bookmark = Bookmark.find(params[:id])
+end
+
+def list_params
+  params.require(:bookmark).permit(:comment, :movie_id)
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true
   validates :title, :overview, presence: true
+  has_many :lists, through: :bookmarks, dependent: :destroy
 end
